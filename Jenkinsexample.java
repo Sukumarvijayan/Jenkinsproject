ccc4d2b785ebb09015ad8e3fef81c287bf8c3826
Sukumarvijayan <Sukumarvijayan1990@gmail.com>
2025-04-02 16:52:53 +0530

    Update Jenkinsexample12.java

--- Jenkinsexample.java
+++ Jenkinsexample.java
@@ -4,5 +4,6 @@ public static void main(String arg[]) {
   System.out.println("Hello world");
   System.out.println("Hello Suku");
   System.out.println("Hello kirubha")
+    System.out.println("Hello krishvanth")
 }
 }
