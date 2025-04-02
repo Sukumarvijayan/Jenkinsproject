ff8953caf534023702ddd928ac2bc6320d983377
Sukumarvijayan <Sukumarvijayan1990@gmail.com>
2025-04-02 17:00:08 +0530

    Update Jenkinsexample34.java

--- Jenkinsexample.java
+++ Jenkinsexample.java
@@ -4,6 +4,7 @@ public static void main(String arg[]) {
   System.out.println("Hello world");
   System.out.println("Hello Suku");
   System.out.println("Hello kirubha")
-    System.out.println("Hello krishvanth")
+  System.out.println("Hello krishvanth")
+  System.out.println("Hello krishitha")
 }
 }
